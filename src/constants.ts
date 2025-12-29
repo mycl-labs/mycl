8ce666c34425afc70b2eb6e2f07bf9397c8ac62a
mycl-labs <mycl-labs@users.noreply.github.com>
2025-12-29 11:46:02 +0900

    expand protocol constants with photon rpc and batch size

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,5 @@
 export const PROTOCOL_NAME = 'MYCL';
 export const DEFAULT_RPC = 'https://api.mainnet-beta.solana.com';
+export const LIGHT_PHOTON_RPC = 'https://mainnet.helius-rpc.com';
+export const FEED_ROOT_SEED = 'mycl_feed_root_v1';
+export const MAX_BATCH_SIZE = 256;
