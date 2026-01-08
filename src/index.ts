22ae51a41700755c3fcc34bbbf175d0674036308
mycl-labs <mycl-labs@users.noreply.github.com>
2026-01-08 10:50:39 +0900

    re-export every public surface

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
 export { HyphaClient } from './client';
-export { PROTOCOL_NAME } from './constants';
+export type { HyphaClientOptions } from './client';
+export { Feed } from './feed';
+export { SubscriptionManager } from './subscribe';
+export type { SampleHandler } from './subscribe';
+export { compress, decompress } from './compression';
+export type { CompressedPayload } from './compression';
+export { verifyEnvelope, describeEnvelope } from './proof';
+export type { ProofEnvelope } from './proof';
+export { rpcCall } from './transport';
+export type { TransportOptions } from './transport';
+export { FeedCategory, FeedSampleSchema, FeedMetadataSchema, FeedBatchSchema } from './schema';
+export type { FeedSample, FeedMetadata, FeedBatch } from './schema';
+export { HyphaError, CompressionError, ProofError, TransportError } from './errors';
+export {
+  PROTOCOL_NAME,
+  PROTOCOL_VERSION,
+  DEFAULT_RPC,
+  LIGHT_PHOTON_RPC,
+  FEED_ROOT_SEED,
+  MAX_BATCH_SIZE,
+} from './constants';
