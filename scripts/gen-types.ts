8add5405f05d6b418ce619a0063bdb310d2cb94a
mycl-labs <mycl-labs@users.noreply.github.com>
2026-01-19 15:51:12 +0900

    add category enumerator generator

---
+++ scripts/gen-types.ts
@@ -0,0 +1,5 @@
+import { FeedCategory } from '../src/schema';
+
+const categories = FeedCategory.options;
+console.log('// generated category list');
+for (const c of categories) console.log(`export const CATEGORY_${c.toUpperCase()} = ${JSON.stringify(c)};`);
